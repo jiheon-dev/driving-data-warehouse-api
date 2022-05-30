824d2e896c0e49d596aaa6f3b324968cadb68c72
Jiheon Choi <jiheon.unidev@gmail.com>
2022-05-30 18:00:32 +0900

    feat: Load the unstructured data with Pandas

--- sample-driving-data/load-data.py
+++ sample-driving-data/load-data.py
@@ -1,17 +1,17 @@
 import pandas as pd
 from os.path import exists
 import json
+import time
 
 from pymongo import MongoClient
-# TODO: Clean the missing values in datasets
 
 def get_data_frame():
     return pd.read_csv('./datasets/ngsim-data.csv')
 
 def get_database():
-    client = MongoClient(host = 'localhost', port = 27027)
+    client = MongoClient(host = 'localhost', port = 27017)
     database = client['driving-data']
-    return None
+    return database
 
 def get_column_title(df):
     return df.columns.values.tolist()
@@ -29,8 +29,10 @@ def get_result_data(columns, data = None):
     return json.dumps({"columns": columns, "data": data})
 
 if __name__ == "__main__":
+    start = time.time()
     df = get_data_frame()
     is_file_exists = exists("newfile.json")
+    driving_data = get_database()
     
     if not is_file_exists:
         make_file(df.to_json(orient = 'records'))
@@ -39,7 +41,9 @@ if __name__ == "__main__":
     # result = get_result_data(column_title, get_json_data())
     for index, row in df.iterrows():
         print(index)
-
-        print(row.to_json())
+        # driving_data["ngsim"].insert_one(row.to_dict())
     # json = get_json_data()
+    end = time.time()
+    print(format(end-start))
+
     # print(json[0])
